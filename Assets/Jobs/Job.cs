9201e45d9fd92cc3eab16bd95559eae208790dea
Sydney Bridges <sybrid@gmail.com>
2018-01-11 16:43:41 -0500

    Sketching out job structure

---
+++ Assets/Jobs/Job.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Job  {
+    protected string _jobTypeName;
+
+    protected Block targetBlock;
+
+    public string JobType
+    {
+        get { return _jobTypeName; }
+    }
+
+    public virtual void DoJob()
+    {
+        Debug.Log("Working job " + _jobTypeName);
+    }
+
+    public virtual Block GetTargetBlock()
+    {
+        return targetBlock;
+    }
+}
---
+++ Assets/Jobs/JobList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobList  {
+    Job _jobType;
+    Queue<Job> availableJobs;
+    Dictionary<Job, Character> assignedJobs;
+
+    public JobList (Job jobType)
+    {
+        _jobType = jobType;
+    }
+
+    public Job GetNextJobInQueue(Character worker)
+    {
+        if (availableJobs.Count == 0)
+            return null;
+        else
+        {
+            Job job = availableJobs.Dequeue();
+            if (assignedJobs == null)
+                assignedJobs = new Dictionary<Job, Character>();
+
+            assignedJobs.Add(job, worker);
+            return job;
+        }
+    }
+
+    public bool AddJobToQueue(Job job)
+    {
+        if (job.JobType != _jobType.JobType)
+        {
+            return false;
+        }
+        else
+        {
+            availableJobs.Enqueue(job);
+            return true;
+        }
+    }
+}
