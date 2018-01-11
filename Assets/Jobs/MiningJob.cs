c6d30c6c50b4f8b4cfdaf93cbd276ad39a619d79
Sydney Bridges <sybrid@gmail.com>
2018-01-11 16:55:19 -0500

    Mining Job Creation
    Still doesn't do anything

---
+++ Assets/Jobs/MiningJob.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningJob : Job {
+
+    public MiningJob(Block block)
+    {
+        _jobTypeName = "MINING";
+        targetBlock = block;
+    }
+}
